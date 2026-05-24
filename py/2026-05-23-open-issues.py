e580ed3b6dfdd88bed0c1ba5830ccdede48b662e
creativecoding <dalinacascota14@gmail.com>
2026-05-24 23:47:50 +0300

    docs: replace long description with challenge link in Open Issues

--- py/2026-05-23-open-issues.py
+++ py/2026-05-23-open-issues.py
@@ -1,11 +1,8 @@
 """
 Challenge: Open Issues
-Description: Given an array of issue numbers and another array of pull request (PR) numbers, return an array of issues that remain open after all PRs have been merged.
-A PR closes an issue if their digits are a rotation of each other. For example, issue 123 would be closed by PR 231 or 312.
-A PR does not close an issue with the exact same number. For example, PR 123 does not close issue 123. So an issue with all the same number can't get closed.
-Either number may have leading zeros stripped. For example, PR 201 would close issue 12 (012, a rotation of 201). Similarly, issue 201 would be closed by PR 12.
-Return the remaining open issues in the order they were given.
+Link: https://www.freecodecamp.org/learn/daily-coding-challenge/2026-05-23
 """
+
 def get_open_issues(issues, prs):
     open_issues = []
     
