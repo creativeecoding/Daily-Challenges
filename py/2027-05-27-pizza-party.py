6cbd17b726a809961f7236196a65b0082cb06390
creativecoding <dalinacascota14@gmail.com>
2026-05-28 22:31:34 +0300

    feat: add Pizza Party challenge solution

---
+++ py/2027-05-27-pizza-party.py
@@ -0,0 +1,10 @@
+import math
+
+"""
+Challenge: Pizza Party
+Link: https://www.freecodecamp.org/learn/daily-coding-challenge/2026-05-27
+"""
+
+def get_pizzas_to_order(hours_worked):
+    total_slices = sum(max(2, math.ceil(hours / 3)) for hours in hours_worked)
+    return math.ceil(total_slices / 8)
