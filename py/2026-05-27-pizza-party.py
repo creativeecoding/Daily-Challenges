6d491eef3672b2801bffe35f7ad880f7fd55c090
creativecoding <dalinacascota14@gmail.com>
2026-06-01 00:13:34 +0300

    style: reorder file structure for better readability

--- py/2026-05-27-pizza-party.py
+++ py/2026-05-27-pizza-party.py
@@ -1,9 +1,9 @@
-import math
-
 """
 Challenge: Pizza Party
 Link: https://www.freecodecamp.org/learn/daily-coding-challenge/2026-05-27
 """
+import math
+
 
 def get_pizzas_to_order(hours_worked):
     total_slices = sum(max(2, math.ceil(hours / 3)) for hours in hours_worked)
